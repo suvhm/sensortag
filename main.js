058d6610f67e7a9fca450618226b1f843a1bc460
Shubham Pushpkar <shubham2.pushpkar@aricent.com>
2018-09-24 12:58:42 +0530

    changes in uri

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ if (delayInObs == undefined) {
 
 // Creating an object of StarfishService to communicate with the Starfish Studio platform
 const options = {
-        'endpoint' : 'https://poc.api.ssniot.cloud',
+        'endpoint' : 'https://api.data-platform.developer.ssni.com',
         'credentials' : {
          'clientId' : process.argv[2],
          'clientSecret' : process.argv[3]
